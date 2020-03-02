4243fb425d42fdc1fe97467d0f903c61bdcc1384
Oskar Jönsson <peroskarjonsson@gmail.com>
2020-03-02 11:46:41 +0100

    added a functional component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,34 @@ import React from 'react';
 import SearchCity from './components/SearchCity';
 import WeatherReport from './components/WeatherReport';
 
+
 class App extends React.Component {
-	state = {
-		errorMessage: false,
-		report: null,
+	constructor(){
+		super()
+		this.state = {
+			errorMessage: false,
+			report: true,
+			city: "",
+			country: "",
+			temperature: "",
+			humidity: ""
+		}
 	}
+
+	getWeather = async (id) => {
+		const APIKey = "a9f6719e37f20890ebff5d91724dec1f";
+		const call = await fetch(
+			`http://api.openweathermap.org/data/2.5/weather?q=${id}&units=metric&appid=${APIKey}`
+		);
+		const response = await call.json();
+		console.log(response);
+
+		this.setState({
+			city: response.name,
+			temperature: response.main.temp,
+			humidity: response.main.humidity
+		})
+	};
 
 	render() {
 		return (
@@ -16,12 +39,12 @@ class App extends React.Component {
 						<span role="img" aria-label="Weather?">🌦❔</span>
 					</h1>
 
-					<SearchCity />
+					<SearchCity item={this.state} getWeather={this.getWeather}/>
 
 					{
 						this.state.report
 						? (
-							<WeatherReport />
+							<WeatherReport item={this.state}/>
 						)
 						: ''
 					}
--- src/components/SearchCity.js
+++ src/components/SearchCity.js
@@ -1,12 +1,40 @@
 import React from 'react';
 
 class SearchCity extends React.Component {
+
+	state = {
+		cityName: '',
+	}
+
+	handleFormSubmit = (e) => {
+		e.preventDefault();
+
+		this.props.getWeather(this.state.cityName);
+
+		this.setState({
+			cityName: '',
+		});
+	}
+
+	handleInputTitleChange = (e) => {
+		console.log(e.target.value)
+		this.setState({
+			cityName: e.target.value,
+		});
+	}
+
 	render() {
 		return (
 			<div id="SearchCity" className="mb-5">
-				<form>
+				<form onSubmit={this.handleFormSubmit}>
 					<div className="input-group">
-						<input type="text" className="form-control form-control-lg" id="city" />
+						<input 
+							type="text" 
+							className="form-control form-control-lg" 
+							id="city" 
+							onChange={this.handleInputTitleChange}
+							value={this.state.cityName}
+						/>
 
 						<div className="input-group-append">
 							<button className="btn btn-success btn-lg">Search</button>
